e98a2e4eec68a65356b6597865eebe11f66072ac
diaabadr <diaabadr82@gmail.com>
2023-03-29 11:02:29 +0200

    refactoring code

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,8 @@
-import { registerCommands } from "./utils/utilities";
-import { DeploymentsViewProvider } from "./webviews/views/deployments.view.provider";
-import { HomeViewProvider } from "./webviews/views/home.view.provider";
 import * as vscode from "vscode";
-import { LandingPageViewProvider } from "./webviews/views/landingPage.view.provider";
-import { HelpsTreeItem } from "./tree.views/Help.Feedback.Item";
-import { AzExtTreeDataProvider } from "@microsoft/vscode-azext-utils";
 import { extVars } from "./statics/extension.variables";
-
-// interface MetacallState extends vscode.Memento {
-//   isFirstRun?: boolean;
-// }
+import { registerTrees } from "./registeration/register.tree.views";
+import { registerWebViews } from "./registeration/register.web.views";
+import { registerCommands } from "./registeration/register.commands";
 
 export async function activate(context: vscode.ExtensionContext) {
   vscode.window.showInformationMessage("Hello World from metacall!");
@@ -20,59 +13,11 @@ export async function activate(context: vscode.ExtensionContext) {
   // registering all commands
   registerCommands(context);
 
-  // Check if this is the first time the extension is being run
-  const extensionState = context.globalState.get("metacall.extensionState");
+  // registering all trees
+  registerTrees(context);
 
-  // if first time, show the landing page with documenation and instructions
-  if (!extensionState) {
-    const panel = vscode.window.createWebviewPanel(
-      "metacall.landingPage",
-      "Welcome to Metacall",
-      vscode.ViewColumn.One,
-      {
-        enableScripts: true,
-      }
-    );
-
-    const landingPageViewProvider = new LandingPageViewProvider(
-      panel,
-      context.extensionUri
-    );
-    context.subscriptions.push(landingPageViewProvider);
-    context.globalState.update("metacall.extensionState", true);
-  }
-
-  const homeViewProvider = new HomeViewProvider(context.extensionUri);
-  const deploymentsViewProvider = new DeploymentsViewProvider(
-    context.extensionUri
-  );
-
-  context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider(
-      HomeViewProvider.viewType,
-      homeViewProvider
-    )
-  );
-
-  context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider(
-      DeploymentsViewProvider.viewType,
-      deploymentsViewProvider
-    )
-  );
-
-  const helpRoot = new HelpsTreeItem(undefined);
-  const helpTreeDataProvider = new AzExtTreeDataProvider(
-    helpRoot,
-    "metacall.helpView"
-  );
-
-  const helpTreeView = vscode.window.createTreeView(
-    "metacall.helpAndFeedbackView",
-    { treeDataProvider: helpTreeDataProvider, canSelectMany: false }
-  );
-
-  context.subscriptions.push(helpTreeView);
+  // registering all web views
+  registerWebViews(context);
 }
 
 export function deactivate() {
---
+++ src/registeration/register.commands.ts
@@ -0,0 +1,102 @@
+import * as vscode from "vscode";
+import { createNewTask } from "../utils/utilities";
+import { OpenUrlTreeItem } from "../views/tree.views/OpenUrlTreeItem";
+
+export const registerCommands = (context: vscode.ExtensionContext) => {
+  const helloWorldCommand = vscode.commands.registerCommand(
+    "metacall.helloWorld",
+    () => {
+      vscode.window.showInformationMessage(
+        "Hello World from metacall! Let's deploy...🚀"
+      );
+    }
+  );
+
+  const helpCommand = vscode.commands.registerCommand(
+    "metacall.help",
+    async () => {
+      const helpTask: vscode.Task = createNewTask(
+        "shell.Help",
+        "Help Terminal",
+        "metacall.help",
+        "metacall-deploy --help"
+      );
+
+      try {
+        await vscode.tasks.executeTask(helpTask);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  );
+
+  const deployCommand = vscode.commands.registerCommand(
+    "metacall.createDeploy",
+    async () => {
+      const deployTask: vscode.Task = createNewTask(
+        "shell.Deploy",
+        "Deploy Terminal",
+        "metacall.createDeploy",
+        "metacall-deploy"
+      );
+
+      try {
+        await vscode.tasks.executeTask(deployTask);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  );
+
+  const logoutCommand = vscode.commands.registerCommand(
+    "metacall.logout",
+    async () => {
+      const logoutTask: vscode.Task = createNewTask(
+        "shell.Logout",
+        "Logout Terminal",
+        "metacall.logout",
+        "metacall-deploy --logout"
+      );
+
+      try {
+        await vscode.tasks.executeTask(logoutTask);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  );
+
+  const installMetacallCLICommand = vscode.commands.registerCommand(
+    "metacall.installCLI",
+    async () => {
+      const installMetacallCLITask: vscode.Task = createNewTask(
+        "shell.InstallCLI",
+        "Install Metacall CLI Terminal",
+        "metacall.installCLI",
+        "npm i -g @metacall/deploy"
+      );
+
+      try {
+        await vscode.tasks.executeTask(installMetacallCLITask);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  );
+
+  const openUrlCommand = vscode.commands.registerCommand(
+    "metacall.openUrl",
+    async (item: OpenUrlTreeItem) => {
+      await item.openUrl();
+    }
+  );
+
+  context.subscriptions.push(
+    helloWorldCommand,
+    helpCommand,
+    deployCommand,
+    logoutCommand,
+    installMetacallCLICommand,
+    openUrlCommand
+  );
+};
