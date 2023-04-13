4e268907cbfdd4db287a1388271e4f50e33a9316
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2023-04-13 14:47:59 +0100

    install-checker Class

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -1,6 +1,8 @@
 import path = require("path");
 import { extVars } from "../statics/extension.variables";
 import * as vscode from "vscode";
+import * as child_process from 'child_process';
+
 export function getIconPath(iconName: string): string {
   return path.join(getResourcesPath(), iconName);
 }
@@ -24,3 +26,24 @@ export const createNewTask = (
       new vscode.ShellExecution(command)
     );
   };
+
+  export class InstallCheck {
+    public static async checkInstall(): Promise<boolean> {
+      try {
+        const version = child_process.execSync('metacall-deploy --version').toString().trim();
+        if (/^\d+\.\d+\.\d+$/.test(version)) {
+          vscode.window.showInformationMessage('Metacall is installed!');
+          return true;
+        } else {
+          return false;
+        }
+      } catch (error: any) {
+        vscode.window.showWarningMessage('Metacall is not installed', 'Install Metacall').then((selection) => {
+          if (selection === 'Install Metacall') {
+            vscode.env.openExternal(vscode.Uri.parse('https://github.com/metacall/deploy#readme'));
+          }
+        });
+        return false;
+      }
+    }
+  }
